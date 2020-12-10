fbc6cb1216ae955b2d28fda5617b2ef1fcc2ee6d
Mark <fqchenxin@gmail.com>
2020-12-10 22:57:20 +1100

    add webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+    }),
+    // new UglifyJsPlugin(),
+  ],
+  devServer: {
+    contentBase: './dist',
+    port: '8080',
+    host: 'localhost',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.scss/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.jsx?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/react'],
+              plugins: [
+                ['@babel/plugin-proposal-decorators', { legacy: true }],
+              ],
+            },
+          },
+        ],
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
